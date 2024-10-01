8488297698424b1ecfba123e8f65de0cbe90da3a
Ronald Thayil <montythayil@gmail.com>
2024-10-01 20:54:45 +0530

    Added Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import Link from "next/link";
+import { Menu, X } from "lucide-react";
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
