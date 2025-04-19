680325f31aa9ba2d833e1075892cf32d49a5ec19
Monty Thayil <montythayil@gmail.com>
2025-04-19 18:37:04 +0530

    Added for Quatation

--- src/app/quotation/quotationTemplate.js
+++ src/app/quotation/quotationTemplate.js
@@ -96,7 +96,7 @@ export const QuotationTemplate = (props) => {
       </div>
       <div class="quotation">QUOTATION ORDER</div>
       <div style="display: flex; justify-content: space-between;">
-         <p><strong>QO No.:</strong> ${props.qoNo}</p>
+         <p><strong>QO No.:</strong> ${props.quotationId}</p>
          <p><strong>Date:</strong> ${props.date}</p>
       </div>
       <div style="display: flex; justify-content: space-between; margin-bottom: 15px;">
